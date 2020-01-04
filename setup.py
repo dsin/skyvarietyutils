180dc484dbdf1ea542d24945051df68489440e7a
Phongsathorn Eakamongul <phongsathorn.eakamongul@rockyou.com>
2020-01-04 10:29:03 +0700

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='skyvarietyutils',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:dsin/skyvarietyutils.git',
+    author='dsin',
+    author_email='dsin@skyvariety.com',
+    license='unlicense',
+    packages=['skyvarietyutils'],
+    zip_safe=False
+)
