1cfd248b982e29f4c247406bcd888478161ea6bb
Phongsathorn Eakamongul <phong.ek@gmail.com>
2020-05-24 20:39:28 +0700

    testing

---
+++ skyvarietyutils/__init__.py
@@ -0,0 +1,16 @@
+def hello():
+  print('It works!')
+
+from . import (
+  calculator,
+  captcha,
+  http,
+  mail,
+  performance,
+  siamsee,
+  string,
+  twitterads,
+  twitter,
+  weibo,
+  include.sinaweibopy.snspy,
+)
