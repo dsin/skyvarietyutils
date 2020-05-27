b2b8502da903d0ab1cb7e72407a69898a3bafca7
Phongsathorn Eakamongul <phong.ek@gmail.com>
2020-05-27 19:31:07 +0700

    make sure they use latest version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='skyvarietyutils',
-    version='0.0.2',
+    version='0.0.1',
     description='My private package from private github repo',
     url='git@github.com:dsin/skyvarietyutils.git',
     author='dsin',
