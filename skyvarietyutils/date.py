82f2b7a2219784873280ebbe80a33bff0cb26ae0
Phongsathorn Eakamongul <phong.ek@gmail.com>
2020-07-16 08:46:50 +0700

    tomorrow at

--- skyvarietyutils/date.py
+++ skyvarietyutils/date.py
@@ -1,7 +1,11 @@
-import datetime
+import datetime, random
 
 def daysSinceEpoch():
   epoch = datetime.datetime.utcfromtimestamp(0) # ( 1970-01-01 00:00:00 )
   today = datetime.datetime.today() # 13196 days, 9:50:44.266200
   d = today - epoch
   return d.days # timedelta object ( 13196 )
+
+def tomorrowAt(hour):
+  now = datetime.datetime.now()
+  return datetime.datetime(now.year, now.month, now.day+1, hour-7, random.randint(0,59), random.randint(0,59), tzinfo=datetime.timezone.utc)
