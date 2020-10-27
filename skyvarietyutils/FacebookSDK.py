6437ad07d981179f13b4da4db2a06c2f2393f599
Phongsathorn Eakamongul <phong.ek@gmail.com>
2020-10-27 20:52:26 +0700

    facebookSDK

--- skyvarietyutils/FacebookSDK.py
+++ skyvarietyutils/FacebookSDK.py
@@ -1,4 +1,5 @@
-import facebook, json
+import facebook, json, urllib
+import skyvarietyutils
 
 # get user access token
 # from https://developers.facebook.com/tools/explorer/
@@ -12,8 +13,62 @@ import facebook, json
 # ref : https://medium.com/@yasithlokuge/how-to-generate-a-never-expiring-facebook-page-access-token-24ac5c1a95f1
 
 class FacebookSDK:
-  def __init__(self, page_access_token):
-    self.graph = facebook.GraphAPI(page_access_token)
+  def __init__(self, access_token, app_id=None, app_secret=None, callback_url=''):
+    self.APP_ID = app_id
+    self.APP_SECRET = app_secret
+    self.CALLBACK_URL = callback_url
+
+    if access_token:
+      self.graph = facebook.GraphAPI(access_token)
+    else :
+      self.graph = facebook.GraphAPI()
+
+  def login(self, scopes):
+    return self.graph.get_auth_url(self.APP_ID, self.CALLBACK_URL, scopes)
+
+  def callback_url_verification(self, code):
+    args = {
+      'client_id': self.APP_ID,
+      'redirect_uri': self.CALLBACK_URL,
+      'client_secret': self.APP_SECRET,
+      'code': code,
+    }
+    access_token = None
+    try:
+      response = json.loads(skyvarietyutils.http.get('https://graph.facebook.com/oauth/access_token', args).read())
+      access_token = response['access_token']
+    except urllib.error.HTTPError as err:
+      print(err.read())
+    return access_token
+
+  def get_long_lived_user_access_token(self, user_access_token):
+    args = {
+      'client_id': self.APP_ID,
+      'client_secret': self.APP_SECRET,
+      'grant_type': 'fb_exchange_token',
+      'fb_exchange_token': user_access_token,
+    }
+    response = json.loads(skyvarietyutils.http.get('https://graph.facebook.com/oauth/access_token', args).read())
+    access_token = response['access_token']
+    return access_token
+
+  def me(self, fields):
+    return self.graph.get_object('me', fields=fields)
+
+  # def get_page_access_token(self, page_id):
+  #   return self.graph.get_object('page')
+
+  # # https://developers.facebook.com/docs/facebook-login/access-tokens/refreshing/
+  # def get_long_lived_page_access_token(self, user_id, long_lived_user_access_token):
+  #   args = {
+  #     'access_token': long_lived_user_access_token
+  #   }
+  #   try:
+  #     response = json.loads(skyvarietyutils.http.get('https://graph.facebook.com/'+user_id+'/accounts', args).read())
+  #     data = response['data']
+  #   except urllib.error.HTTPError as err:
+  #     print(err.read())
+  #   return data
 
   def postImages(self, *args, **kwargs):
     page_id = args[0]
